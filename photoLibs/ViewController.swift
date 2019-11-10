a5acb06e32bb178bdd266a13b5424739a0f0b1df
Konstantin Iroshnikov <k.iroshnikov@itmegastar.com>
2019-11-10 12:40:30 +0700

    init commit

---
+++ photoLibs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoLibs
+//
+//  Created by Username on 10.11.2019.
+//  Copyright © 2019 Username. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
